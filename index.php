017c29118cdfac399c28965ae994de0143b51b94
Joris Guesné <joris.dev@proton.me>
2025-01-17 10:49:29 +0100

    Je n'ai rien fait. Pas eu le temps de comprendre le code

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $dotenv->load();
 try {
     $pdo = new PDO(
         'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'],
-        $_ENV['DB_USER'],
+        $_ENV['DB_USERNAME'],
         $_ENV['DB_PASSWORD']
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -17,3 +17,6 @@ try {
 } catch (PDOException $e) {
     die('Erreur de connexion à la base de données : ' . $e->getMessage());
 }
+
+//Le code au dessus à été rédigé par Thomas et permet la connexion a la DB. De plus, il verifie son status.
+require './managers/AuthorManager.php';
