13370f93ea1ab0536175d4bf5cbbde1f8216aa6c
thomasR35 <thomasriou.35@gmail.com>
2025-01-17 10:04:46 +0100

    Feat Class ( Getter /Setters )

--- models/Author.php
+++ models/Author.php
@@ -1,22 +1,39 @@
 <?php
 class Author
 {
-    private int $id;
-    private string $name;
+    private string $first_name;
+    private string $last_name;
+    private string $biography;
 
-    public function __construct(int $id, string $name)
+    public function __construct(string $first_name, string $last_name, string $biography)
     {
-        $this->id = $id;
-        $this->name = $name;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->biography = $biography;
     }
 
-    public function getId(): int
+    public function getFirstName(): string
     {
-        return $this->id;
+        return $this->first_name . " " . $this->last_name;
     }
-
-    public function getName(): string
+    public function setFirstName(string $first_name): void
     {
-        return $this->name;
+        $this->first_name = $first_name;
+    }
+    public function getLastName(): string
+    {
+        return $this->last_name;
+    }
+    public function setLastName(string $last_name): void
+    {
+        $this->last_name = $last_name;
+    }
+    public function getBiography(): string
+    {
+        return $this->biography;
+    }
+    public function setBiography(string $biography): void
+    {
+        $this->biography = $biography;
     }
 }
--- models/Book.php
+++ models/Book.php
@@ -3,15 +3,20 @@ class Book
 {
     private int $id;
     private string $title;
+    private string $excerpt;
+    private float $price;
     private int $authorId;
 
-    public function __construct(int $id, string $title, int $authorId)
+    public function __construct(int $id, string $title, string $excerpt, float $price, int $authorId)
     {
         $this->id = $id;
         $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->price = $price;
         $this->authorId = $authorId;
     }
 
+    // Getters
     public function getId(): int
     {
         return $this->id;
@@ -22,8 +27,38 @@ class Book
         return $this->title;
     }
 
+    public function getExcerpt(): string
+    {
+        return $this->excerpt;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
     public function getAuthorId(): int
     {
         return $this->authorId;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function setExcerpt(string $excerpt): void
+    {
+        $this->excerpt = $excerpt;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function setAuthorId(int $authorId): void
+    {
+        $this->authorId = $authorId;
     }
 }
